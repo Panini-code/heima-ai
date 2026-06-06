a7e3df6b2fde07542f1ca34570c251243e8a1599
wang-haoran777 <1244663920@qq.com>
2026-06-06 17:25:10 +0800

    增加会话删除功能

--- check_router.py
+++
@@ -1,2 +0,0 @@
-﻿with open("src/main/resources/static/spring-ai-protal/src/router/index.js", "r", encoding="utf-8") as f:
-    print(f.read())
--- verify_cs.py
+++
@@ -1,7 +0,0 @@
-﻿with open("src/main/resources/static/spring-ai-protal/src/views/CustomerService.vue", "r", encoding="utf-8") as f:
-    content = f.read()
-template = content.split("<script")[0]
-if "delete-btn" in template and "history-item-content" in template and "modal-overlay" in template:
-    print("CustomerService: ALL GOOD")
-else:
-    print("CustomerService: ISSUES FOUND")
